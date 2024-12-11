d52b82ddf3bdec6f090b3994534f020a1cad215c
thang <thang.nguyen@flex.com.vn>
2024-12-11 10:11:21 +0700

    facility-test

---
+++ service-worker-getting-started/src/main.ts
@@ -0,0 +1,19 @@
+import { bootstrapApplication, provideProtractorTestingSupport } from '@angular/platform-browser';
+
+import { AppComponent } from './app/app.component';
+import { provideServiceWorker } from '@angular/service-worker';
+import { isDevMode } from '@angular/core';
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideProtractorTestingSupport(),
+    provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    }),
+    provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })
+]
+});
